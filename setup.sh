7dd4035811a736df0aba86f1c99e1b2532955967
Mark Carlebach <markcarlebach@Marks-MacBook-Pro.local>
2025-01-14 08:16:55 -0500

    partially done

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 python -m venv venv
 source venv/bin/activate  # On Windows use: venv\Scripts\activate 
+
+# Install PyTorch first
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu
+
+# Then install other requirements
+pip install -r requirements.txt 
